14d1dd0569080f709aad475558fa1bf2ab9dc22c
Bhaggyjoshi <bhagyajoshi329@gmail.com>
2022-03-09 22:55:46 +0530

    Factorial program using function.

---
+++ FUNCTIONS/Factorial.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Factorial {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int r = sc.nextInt();
+
+        int nf = Fact(n);
+        int rf = Fact(r);
+        int nmrf = Fact(n-r);
+        int ncr = nf/(nmrf*rf);
+
+        System.out.print(ncr);
+    }
+    public static int Fact(int x) {
+        int xf = 1;
+        for (int i = 1; i <=x ; i++) {
+            xf *= i;
+        }
+        return xf;
+    }
+}
