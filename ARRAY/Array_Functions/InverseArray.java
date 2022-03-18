6e0e3062e2e7ee091e98a9f6eed895e5a49e78d0
Bhaggyjoshi <bhagyajoshi329@gmail.com>
2022-03-18 21:31:25 +0530

    Adding new java program inverse of array

---
+++ ARRAY/Array_Functions/InverseArray.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+/**
+ * InverseArray
+ */
+public class InverseArray {
+
+    public static void main(String[] args) {
+        Scanner sc  = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = sc.nextInt();
+        }
+        Inverse(arr);
+
+    }
+    private static void Inverse(int[] a) {
+        
+        int[] inv = new int[a.length];
+        for (int i = 0; i < a.length; i++) {
+            int pos = a[i];
+            inv[pos] = i;
+        }
+        
+        for (int i = 0; i < inv.length; i++) {
+            System.out.println(inv[i]);
+        }
+    }
+}
