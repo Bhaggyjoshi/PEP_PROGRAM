d0dfcc386c26ef3adf64d587c9a719d2145ce692
Bhaggyjoshi <bhagyajoshi329@gmail.com>
2022-03-18 16:07:41 +0530

    Adding new file find element in array using function and rotate an array without using function

---
+++ ARRAY/Array_Functions/SpanArray.java
@@ -0,0 +1,33 @@
+package Array_Functions;
+
+import java.util.Scanner;
+
+public class SpanArray {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n  = sc.nextInt();
+        int[] arr  =  new int[n];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = sc.nextInt();
+
+        }
+        int res = Span(arr);
+        System.out.print(res);
+        
+    }
+    public static int Span(int[] array) {
+        int ans = 0;
+        int max = array[0];
+        int min = array[0];
+        for (int i = 0; i < array.length; i++) {
+            if(array[i] > max){
+                max = array[i];
+            }
+            if (array[i] < min) {
+                min = array[i];
+            }
+        }
+        ans = max-min;
+        return ans;
+    }
+}
