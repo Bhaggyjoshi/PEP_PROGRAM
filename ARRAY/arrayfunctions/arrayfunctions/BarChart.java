b512bcd02d00020be5a1c19d05eee0418a3c76fd
Bhaggyjoshi <bhagyajoshi329@gmail.com>
2022-03-19 13:22:50 +0530

    Adding new Barchart java program using function

---
+++ ARRAY/arrayfunctions/arrayfunctions/BarChart.java
@@ -0,0 +1,37 @@
+package arrayfunctions;
+
+import java.util.Scanner;
+
+public class BarChart {
+    public static void main(String[] args) {
+        Scanner scn  =  new Scanner(System.in);
+        int n = scn.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = scn.nextInt(); 
+        }
+        Bar(arr);
+
+    }
+
+    public static void Bar(int[] ans) {
+        int max = ans[0];
+
+        for (int i = 0; i < ans.length; i++) {
+            if (ans[i] > max) {
+                max = ans[i];
+            }
+        }
+
+        for (int i = max; i >=1; i--) {
+            for (int j = 0; j < ans.length; j++) {
+                if (ans[j] >= i) {
+                    System.out.print("*\t");
+                }else{
+                    System.out.print("\t");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
