038c9e9667235d690c9ba717ec79f15870492539
Bhaggyjoshi <bhagyajoshi329@gmail.com>
2022-03-20 23:33:09 +0530

    Rotate array and inverse pyramid program added

---
+++ ARRAY/arrayfunctions/arrayfunctions/RotateArray.java
@@ -0,0 +1,34 @@
+package arrayfunctions;
+
+import java.util.Scanner;
+
+public class RotateArray {
+    public static void main(String[] args) {
+        Scanner sc  = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = sc.nextInt();
+        }
+        int k = sc.nextInt();
+        int [] res = Rotate(arr, k);
+    }
+
+    public static int[] Rotate(int[] a, int k) {
+        int 
+    }
+
+    public static int[] Reverse(int[] arr) {
+        int left  = 0;
+        int right = arr.length-1;
+        for (int i = 0; i < arr.length; i++) {
+            if(left < right){
+                int temp = arr[left];
+                arr[left] = arr[right];
+                arr[right] = temp;
+            }
+        }
+
+        return arr;
+    }
+}
--- PATTERNS/InversePyramid.java
+++ PATTERNS/InversePyramid.java
@@ -4,6 +4,6 @@
 public class InversePyramid {
 
     public static void main(String[] args) {
-        int 
+        
     }
 }
