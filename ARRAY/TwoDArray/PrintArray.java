016fd6727b6be178bf40106cccbf1797c97098b2
Bhaggyjoshi <bhagyajoshi329@gmail.com>
2022-03-30 22:27:42 +0530

    Adding 2D array program using java

---
+++ ARRAY/TwoDArray/PrintArray.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+/**
+ * PrintArray
+ */
+public class PrintArray {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+
+        int[][] arr = new int[n][m];
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr.length; j++) {
+                arr[i][j] = sc.nextInt();
+            }
+        }
+        
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr.length; j++) {
+                System.out.println(arr[i][j]);
+            }
+        }
+    }
+}
