30e466f3760811a38d1e5b7056124c11c23bc0a6
Bhaggyjoshi <bhagyajoshi329@gmail.com>
2022-04-01 14:54:12 +0530

    adding new java program based on 2d array

---
+++ ARRAY/TwoDArray/MatrixMultiple.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class MatrixMultiple {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int sum  = 0;
+        int temp;
+        for (int i = 0; i < args.length; i++) {
+            
+        }
+    }
+}
---
+++ ARRAY/TwoDArray/SumOfmatrix.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class SumOfmatrix {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        int[][] arr1 = new int[n][m];
+       
+        int[][] sum = new int[n][m];
+
+
+        for (int i = 0; i < arr1.length; i++) {
+            for (int j = 0; j < arr1.length; j++) {
+                arr1[i][j]=sc.nextInt();
+            }
+        }
+
+        int[][] arr2 = new int[n][m];
+
+        for (int i = 0; i < arr2.length; i++) {
+            for (int j = 0; j < arr2.length; j++) {
+              arr2[i][j] = sc.nextInt();
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                sum[i][j] = arr1[i][j]+arr2[i][j];
+            }
+        }
+    }
+}
