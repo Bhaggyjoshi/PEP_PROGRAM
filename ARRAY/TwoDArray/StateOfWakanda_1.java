2fe0e3d46c5be0ca19badabbece5b57f5c30956b
Bhaggyjoshi <bhagyajoshi329@gmail.com>
2022-04-03 15:15:50 +0530

    Adding new java program State of Wakanda_1

---
+++ ARRAY/TwoDArray/StateOfWakanda_1.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class StateOfWakanda_1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+
+        int[][] mat = new int[n][m];
+        for (int i = 0; i < mat.length; i++) {
+            for (int j = 0; j < mat.length; j++) {
+                mat[i][j] = sc.nextInt();
+            }
+        }
+
+        for (int i = 0; i < mat[0].length; i++) {
+            if(i%2 == 0){
+                for (int j = 0; j < mat.length; j++) {
+                    System.out.println(mat[i][j]);
+                }
+            }
+            else{
+                for (int j = mat.length - 1; j >= 0; j--)
+                    System.out.println(mat[i][j]);
+                }
+            }
+        }
+}
+
