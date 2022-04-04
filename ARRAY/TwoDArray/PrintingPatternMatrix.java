5c8804b725e2b76550b74ba626012f47804e69a7
Bhaggyjoshi <bhagyajoshi329@gmail.com>
2022-04-04 21:46:56 +0530

    Adding new java program printing matix from right side

---
+++ ARRAY/TwoDArray/PrintingPatternMatrix.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class PrintingPatternMatrix {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+
+        int[][] mat = new int[n][m];
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                mat[i][j] = sc.nextInt();
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            for (int j = m-1; j >= 0  ; j--) {
+                System.out.print(mat[i][j]+" ");
+            }
+            System.out.println();
+        }
+    }
+}
---
+++ ARRAY/TwoDArray/SprialDisplay.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class SprialDisplay {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        int[][] matrix = new int[n][m];
+
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix.length; j++) {
+                matrix[i][j] = sc.nextInt();
+            }
+        }
+
+        
+    }
+}
