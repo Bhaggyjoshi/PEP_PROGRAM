9d06c8defb20318d37fa65636de25c648f3bb53d
Bhaggyjoshi <bhagyajoshi329@gmail.com>
2022-04-04 21:54:33 +0530

    adding new java program where we have print matrix from top to bottom

---
+++ ARRAY/TwoDArray/PrintPatternMatrix_2.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class PrintPatternMatrix_2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+
+        int[][] mat = new int[n][m];
+        for (int i = 0; i < mat.length; i++) {
+            for (int j = 0; j < mat.length; j++) {
+                mat[i][j] = sc.nextInt();
+            }
+        }
+
+        for (int j = 0; j < m; j++) {
+            for (int i = 0; i < n; i++) {
+                System.out.print(mat[i][j]+" ");
+            }
+            System.out.println();
+        }
+
+        sc.close();
+    }
+}
