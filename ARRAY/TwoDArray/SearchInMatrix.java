ac60afa36e78aeb5a443c712e379aa5f5d1e8582
Bhaggyjoshi <bhagyajoshi329@gmail.com>
2022-04-07 21:02:19 +0530

    Java program which helps to find element in the matix

---
+++ ARRAY/TwoDArray/SearchInMatrix.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class SearchInMatrix {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        
+        
+        int[][] mat = new int[n][m];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                mat[i][j] = sc.nextInt();
+            }
+        }
+
+        int data = sc.nextInt();
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (mat[i][j]==data) {
+                    System.out.println(i+ " "+j);
+                    break;
+                }
+                
+            }
+
+        }
+        
+            System.out.println("Not Found");
+        
+
+        sc.close();
+    }
+}
