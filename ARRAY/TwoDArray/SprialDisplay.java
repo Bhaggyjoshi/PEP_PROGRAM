f934811a5520ba24140664dc33582c44313cec99
Bhaggyjoshi <bhagyajoshi329@gmail.com>
2022-06-12 08:50:52 +0530

    Adding new java program spiralDisplay

--- ARRAY/TwoDArray/SprialDisplay.java
+++ ARRAY/TwoDArray/SprialDisplay.java
@@ -13,6 +13,42 @@ public class SprialDisplay {
             }
         }
 
+        int rmin= 0;
+        int rmax = matrix.length-1;
+        int cmin = 0;
+        int cmax = matrix[0].length-1;
+        int tne = matrix.length * matrix[0].length;
+        int count = 0;
+
+
+        while (count <tne) {
+
+            // for left boundary
+            for (int row = rmin; row <= rmax && count < tne; row++) {
+                System.out.println(matrix[row][cmin]);
+                count++;
+            }
+            cmin++;
+            //for bottom boundary
+            for(int col = cmin; col<= cmax && count < tne; col++){
+                System.out.println(matrix[rmax][col]);
+                count++;
+            }
+            rmax--;
+            // for  right boundary
+            for(int row = rmax; row>=rmin && count < tne;row--){
+                System.out.println(matrix[row][cmax]);
+                count++;
+            }
+            cmax--;
+
+            for (int col = cmax; col>=cmin && count < tne; col--) {
+                System.out.println(matrix[rmin][col]);
+                count++;
+            }
+            rmin++;
+        }
+
         sc.close();
     }
 }
